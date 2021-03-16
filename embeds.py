12ec64cc85c26d92f72b1d3de60770fe2270ebc6
rootGuillaume <guillaumegasmi@protonmail.com>
2021-03-16 18:31:19 +0100

    Add comments

--- embeds.py
+++ embeds.py
@@ -3,34 +3,35 @@ import discord
 
 class BotEmbed:
 
+    # Embed | Eender pinned message
     def pinned_embed(msg, description, user):
+
+        # Get message url | Reforge render
         server_id = msg.guild.id
         channel_id = msg.channel.id
         message_id = msg.id
-
         url_message = 'https://discordapp.com/channels/' + str(server_id) + '/' + str(channel_id) + '/' + str(message_id)
 
+        # Get message time | Reforge render
         message_time = str(msg.created_at.time()).split(':')
-
         hours = message_time[0]
         minutes = message_time[1]
-
         message_time = hours + ':' + minutes
 
-
-        embed = discord.Embed(
+        embed = discord.Embed( # Embed Structure
             description=msg.content,
             color=0xf54254
         )
 
-        embed.set_author(
+        embed.set_author( # Embed author details
             name=msg.author.display_name,
             icon_url=msg.author.avatar_url
         )
 
+        # Embed field | Date, Time, Pinned by, Message link
         embed.add_field(name="🗓 Dat️e", value=msg.created_at.date(), inline=True)
         embed.add_field(name="🕓 Time", value=message_time, inline=True)
         embed.add_field(name="📌 Pinned by", value=user.mention, inline=True)
         embed.add_field(name="🔗 Message Link", value=url_message, inline=False)
 
-        return embed
+        return embed # Return embed object
